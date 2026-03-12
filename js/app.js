6dcfbd7e1bfc4ac0228f5df9ae62c418bc5f7cc2
Geaneth <lourdesgeaneth@gmail.com>
2026-03-12 01:24:31 -0500

    Avance_dashboard

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
 function login(){
 
-let email=document.getElementById("email").value
-let pass=document.getElementById("password").value
+let email = document.getElementById("email").value
+let pass = document.getElementById("password").value
 
-if(email=="admin" && pass=="1234")
-{
-window.location="dashboard.html"
+if(email === "admin@bonsai.com" && pass === "1234"){
+
+window.location.href = "dashboard.html"
+
+}else{
+
+alert("Usuario o contraseña incorrectos")
+
 }
-else
-{
-alert("Usuario incorrecto")
+
+}
+
+
+function mostrarPassword(){
+
+let pass = document.getElementById("password")
+
+if(pass.type === "password"){
+pass.type = "text"
+}else{
+pass.type = "password"
 }
 
 }
 
-/////////////////////////////////////////////////////
 
 function actualizar(){
 
 fetch("http://localhost:1880/bonsai")
 
-.then(res=>res.json())
+.then(res => res.json())
 
-.then(data=>{
+.then(data => {
 
-document.getElementById("soil").innerText=data.soil+"%"
-document.getElementById("temp").innerText=data.temp+"°C"
-document.getElementById("pump").innerText=data.pump
+document.getElementById("soil").innerText = data.soil + "%"
+document.getElementById("temp").innerText = data.temp + "°C"
 
 })
 
